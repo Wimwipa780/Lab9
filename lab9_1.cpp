5addeb5be175c18202c5fedd69592a420048bb94
wimwipa780 <wimwipha4746@gmail.com>
2024-12-17 13:54:26 +0700

    Lab9_1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,14 +2,17 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double principal, interestRate, annualPayment, interest, total, newBalance;
+    int year = 1;
+    
 	cout << "Enter initial loan: ";
+	cin >> principal;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interestRate;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cin >> annualPayment;
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +21,33 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+
+while (principal > 0) {
+       interest = principal * (interestRate / 100);
+        total = principal + interest;
+
+     
+        double payment = (total < annualPayment) ? total : annualPayment;
+        newBalance = total - payment;
+
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left <<principal;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << newBalance;
 	cout << "\n";	
 	
-	return 0;
+
+        principal = newBalance;
+
+        if (principal <= 0) break;
+        
+    year++;
+    
+    }
+    	return 0;
 }
